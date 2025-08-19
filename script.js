0cfc24edb9cca8b00a6d435cd6538d6ee6a10417
Ashish Gupta <ashishashishg48@gmail.com>
2025-08-19 08:36:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
  function Car(make, model) {
  	this.make = make
-	 this,model = model
+	 this.model = model
  }
 Car.prototype.getMakeModel = function () {
   return `${this.make} ${this.model}`;
@@ -15,6 +15,10 @@ SportsCar.prototype.constructor = SportsCar;
 SportsCar.prototype.getTopSpeed = function () {
   return this.topSpeed;
 };
+const car = new SportsCar("Ferrari", "Testarossa", 200);
+
+console.log(car.getMakeModel()); 
+console.log(car.getTopSpeed());
 // Do not change the code below
 window.Car = Car;
 window.SportsCar = SportsCar;

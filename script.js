d3b4259a38bfcb1799777c2a80e4a84896b44da9
Ashish Gupta <ashishashishg48@gmail.com>
2025-08-19 08:25:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,40 @@
-// Complete the js code
-function Car(make, model) {}
 
-function SportsCar(make, model, topSpeed) {}
+class Car {
+  constructor(make, model) {
+    this.make = make;
+    this.model = model;
+  }
 
-// Do not change the code below
-window.Car = Car;
-window.SportsCar = SportsCar;
+  getMakeModel() {
+    return `${this.make} ${this.model}`;
+  }
+}
+
+
+class SportsCar extends Car {
+  constructor(topSpeed, make, model) {
+    super(make, model);
+    this.topSpeed = topSpeed;
+  }
+
+  getTopSpeed() {
+    return this.topSpeed;
+  }
+}
+
+
+const car = new SportsCar(200, "Ferrari", "Testarossa");
+
+console.log(car.getMakeModel()); 
+console.log(car.getTopSpeed());  
+
+// function SportsCar(make, model, topSpeed) {
+// 	Car.call(this,make,model)
+// 	this.topSpeed = topSpeed
+// }
+
+// // Do not change the code below
+// window.Car = Car;
+// window.SportsCar = SportsCar;
+// const car = new SportsCar("Toyota","Corolla")
+// console.log(car.getMakeModel());

e01e6997d6b2ce88b1ac777b308f3b19eb580dc1
Ashish Gupta <ashishashishg48@gmail.com>
2025-08-19 08:32:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,40 +1,21 @@
+ function Car(make, model) {
+ 	this.make = make
+	 this,model = model
+ }
+Car.prototype.getMakeModel = function () {
+  return `${this.make} ${this.model}`;
+};
 
-class Car {
-  constructor(make, model) {
-    this.make = make;
-    this.model = model;
-  }
-
-  getMakeModel() {
-    return `${this.make} ${this.model}`;
-  }
+function SportsCar(make, model, topSpeed) {
+	Car.call(this,make,model)
+	this.topSpeed = topSpeed
 }
+SportsCar.prototype = Object.create(Car.prototype);
+SportsCar.prototype.constructor = SportsCar;
+SportsCar.prototype.getTopSpeed = function () {
+  return this.topSpeed;
+};
+// Do not change the code below
+window.Car = Car;
+window.SportsCar = SportsCar;
 
-
-class SportsCar extends Car {
-  constructor(topSpeed, make, model) {
-    super(make, model);
-    this.topSpeed = topSpeed;
-  }
-
-  getTopSpeed() {
-    return this.topSpeed;
-  }
-}
-
-
-const car = new SportsCar(200, "Ferrari", "Testarossa");
-
-console.log(car.getMakeModel()); 
-console.log(car.getTopSpeed());  
-
-// function SportsCar(make, model, topSpeed) {
-// 	Car.call(this,make,model)
-// 	this.topSpeed = topSpeed
-// }
-
-// // Do not change the code below
-// window.Car = Car;
-// window.SportsCar = SportsCar;
-// const car = new SportsCar("Toyota","Corolla")
-// console.log(car.getMakeModel());
